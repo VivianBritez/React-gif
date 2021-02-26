d2a4953a3b01d6b9031450b949d48a60a318201b
Vivian Britez <vivianbritez91@gmail.com>
2021-02-26 12:49:28 -0500

    docs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async ( category )=>{
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=V2DyUsS3dSqK8YX5SqVt5a49Oyv2zdrc&q=${ encodeURI( category )}`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=V2DyUsS3dSqK8YX5SqVt5a49Oyv2zdrc&q=${ encodeURI( category )}`;
     const resp = await fetch( url );
     const  {data} = await resp.json();
     const gifs = data.map( img => {
